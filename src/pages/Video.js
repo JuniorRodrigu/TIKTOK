bfc044182329f14af2af078a3863ae59dd3e0921
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-04-02 11:00:18 -0300

    in

---
+++ src/pages/Video.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./video.css";
+
+function Video() {
+  return(
+  <div className='video'>
+   
+    <video className='video__playar' src="https://youtu.be/7MgJ5OoMwgw?t=7">
+        
+    </video>
+  </div>
+
+  )
+}
+
+
+export default Video;
